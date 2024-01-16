4b14411832e0e7f243869cff050797ed1d3c439f
marvelfan0123 <156744597+marvelfan0123@users.noreply.github.com>
2024-01-16 11:30:03 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+echo "submittedd...";
+/*
+$id = $_POST['id'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+//dbconnection
+$conn = new mysqli('localhost','root','','register');
+if($conn->connect_error){
+    die('connection failed : '.$conn->connect_error);
+}
+else{
+    $stmt=$conn->prepare("insert into registeration(id,fname,lname) values(?,?,?)");
+    $stmt->bind_param("sss",$id,$fname,$lname);
+    $stmt->execute();
+    echo "registered successfully...";
+    $stmt->close();
+    $conn->close();
+}*/
+?>
