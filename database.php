3de73c2a9f2e71b236f7e0ae340499c5fc30c129
learnerZero <156744597+marvelfan0123@users.noreply.github.com>
2024-01-26 08:18:58 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,34 +1,3 @@
 <?php
-echo "Submitted...";
-
-$id = $_POST['id'];
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$loc = $_POST['loc'];
-// dbconnection
-$conn = new mysqli('localhost', 'root', '', 'geo');
-
-// Check connection
-if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
-}
-
-// Prepare and execute SQL statement
-$stmt = $conn->prepare("INSERT INTO test (id, fname, lname, loc) VALUES (?, ?, ?,?)");
-
-// Check if the statement is prepared successfully
-if ($stmt) {
-    $stmt->bind_param("ssss", $id, $fname, $lname, $loc);
-    $stmt->execute();
-
-    echo "Registered successfully...";
-    
-    // Close statement
-    $stmt->close();
-} else {
-    echo "Error preparing statement: " . $conn->error;
-}
-
-// Close connection
-$conn->close();
+echo "Submitted...:
 ?>
